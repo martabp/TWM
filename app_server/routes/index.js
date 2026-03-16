59cab24528f3ae9c8394f490ac47ff63856a6c99
Marta Borrego Padilla <mborrego@aytoalmeria.es>
2026-03-16 13:57:56 +0100

    Añadida pagina about y cambios en index.pug

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -6,3 +6,6 @@ const ctrlMain = require('../controllers/main');
 router.get('/', ctrlMain.index);
 
 module.exports = router;
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
